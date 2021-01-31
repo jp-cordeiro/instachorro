cabce683ad044ff3d5d6ac9f0c8df758083836a9
JP <jpc.araujo@hotmail.com>
2021-01-31 19:22:47 -0300

    add navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import './header.scss'
+import './header.scss';
 
 export default function index() {
   return (
-    <div>
-      
+    <div id="header">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Novo</Link>
+      </nav>
     </div>
-  )
+  );
 }
