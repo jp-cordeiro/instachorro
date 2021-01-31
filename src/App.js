109d1ed91deb55f24f0647f7c11fa7274556f4aa
JP <jpc.araujo@hotmail.com>
2021-01-31 19:18:25 -0300

    config routes and use components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './styles/app.scss';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Routes from './routes';
 
 function App() {
-  return <div id="app"></div>;
+  return (
+    <div id="app">
+      <Header />
+      <Routes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
