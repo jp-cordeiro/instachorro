b3244d62bc107fef791f25da4cdce905c5e80972
JP <jpc.araujo@hotmail.com>
2021-02-02 19:24:20 -0300

    refactoring userContext with login behaviors

--- src/stores/UserStore.jsx
+++ src/stores/UserStore.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import api from '../services/api';
 
 export const UserContext = createContext();
@@ -9,8 +9,33 @@ export default function UserStore({ children }) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const userSession = sessionStorage.getItem('user');
+
+  useEffect(() => {
+    async function autoLogin() {
+      try {
+        setError(null);
+        setIsLoading(true);
+        const token = sessionStorage.getItem('token');
+        if (token) {
+          await getUser();
+          await api.post('/jwt-auth/v1/token/validate');
+          return;
+        }
+        setIsLogged(false);
+      } catch (error) {
+        await userLogout();
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    autoLogin();
+  }, []);
+
   async function userLogin(username, password) {
     try {
+      setError(null);
+      setIsLoading(true);
       const { data } = await api.post('/jwt-auth/v1/token', {
         username,
         password,
@@ -20,18 +45,36 @@ export default function UserStore({ children }) {
       sessionStorage.setItem('token', token);
       await getUser();
     } catch (error) {
-      setError(error);
+      setError(error.message);
+      setIsLogged(false);
+    } finally {
+      setIsLoading(false);
     }
   }
 
+  async function userLogout() {
+    setData(null);
+    setError(null);
+    setIsLoading(false);
+    setIsLogged(false);
+    sessionStorage.removeItem('token');
+  }
+
   async function getUser() {
-    const { data } = await api.get('/api/user');
-    setData(data);
+    if (userSession) {
+      setData(JSON.parse(userSession));
+    } else {
+      const { data } = await api.get('/api/user');
+      setData(data);
+      sessionStorage.setItem('user', JSON.stringify(data));
+    }
     setIsLoading(true);
   }
 
   return (
-    <UserContext.Provider value={{ userLogin }}>
+    <UserContext.Provider
+      value={{ userLogin, userLogout, data, isLoading, isLogged, error }}
+    >
       {children}
     </UserContext.Provider>
   );
