33a08877718eb981ce1f33a07f9979c433493489
JP <jpc.araujo@hotmail.com>
2021-02-02 19:25:23 -0300

    user info of userContext

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import { UserContext } from '../../stores/UserStore';
 
 import { ReactComponent as DogsLogo } from '../../assets/dogs.svg';
 
 import './header.scss';
 
-export default function index() {
+export default function Header() {
+  const { data } = useContext(UserContext);
+
   return (
     <header id="header">
       <nav>
         <Link className="logo" to="/" aria-label="Dogs - Home">
           <DogsLogo />
         </Link>
-        <Link className="login" to="/login">
-          Login / Novo
-        </Link>
+
+        {data ? (
+          <Link className="login" to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className="login" to="/login">
+            Login / Novo
+          </Link>
+        )}
       </nav>
     </header>
   );
