1ea556bc72755ad722486461fa666cf16b715c3c
JP <jpc.araujo@hotmail.com>
2021-02-03 18:19:46 -0300

    LoginCreate

--- src/components/Login/LoginCreate/index.jsx
+++ src/components/Login/LoginCreate/index.jsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import Button from '../../Form/Button';
+import Input from '../../Form/Input';
+import { UserContext } from '../../../stores/UserStore';
 
-import './login-create.scss'
+import './login-create.scss';
+import AppError from '../../../utils/AppError';
+import useFom from '../../../custom-hooks/useFom';
+import { useHistory } from 'react-router-dom';
 
-export default function LoginCreate({ match }) {
-  return <div>Criar</div>;
+export default function LoginCreate() {
+  const { createUser, userLogin, isLoading, error } = useContext(UserContext);
+  const history = useHistory();
+
+  const username = useFom();
+  const email = useFom();
+  const password = useFom();
+
+  const cleanForm = useCallback(() => {
+    const valueReset = {
+      target: {
+        value: '',
+      },
+    };
+    username.onChange(valueReset);
+    email.onChange(valueReset);
+    password.onChange(valueReset);
+  }, [username, email, password]);
+
+  const handleSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+      try {
+        await createUser(username.value, email.value, password.value);
+        await userLogin(username.value, password.value);
+        history.push('/conta');
+      } catch (error) {
+      } finally {
+        cleanForm();
+      }
+    },
+    [createUser, userLogin, cleanForm, history, username, email, password]
+  );
+
+  return (
+    <section id="login-create">
+      <h1>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="username"
+          type="text"
+          label="Usuário"
+          required
+          {...username}
+        />
+        <Input name="email" type="email" label="E-mail" required {...email} />
+        <Input
+          name="password"
+          type="password"
+          label="Senha"
+          required
+          {...password}
+        />
+        {isLoading ? (
+          <Button disabled>Carregando ...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        {error && <AppError error={error} />}
+      </form>
+    </section>
+  );
 }
