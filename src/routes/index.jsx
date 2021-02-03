407404f8fea7c8795ff00a15f06a4be67907eb39
JP <jpc.araujo@hotmail.com>
2021-02-03 18:18:58 -0300

    add usePrevious anda useLocation chage hooks for clean error state in UserStore

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { UserContext } from '../stores/UserStore';
+
 import Home from '../pages/Home';
 import LoginRoutes from './LoginRoutes';
 
-export default function index() {
+const usePrevious = (value) => {
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+};
+
+const useLocationChange = (action) => {
+  const location = useLocation();
+  const prevLocation = usePrevious(location);
+  React.useEffect(() => {
+    action(location, prevLocation);
+  }, [location, action, prevLocation]);
+};
+
+export default function Router() {
+  const { cleanError } = useContext(UserContext);
+
+  useLocationChange((location, prevLocation) => {
+    if (prevLocation && prevLocation.pathname !== location.pathname) {
+      cleanError(null);
+    }
+  });
+
   return (
     <Switch>
       <Route exact path="/" component={Home} />
