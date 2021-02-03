77b16b87a4701ce483ebcd4f4c8c7e3273cb9e0c
JP <jpc.araujo@hotmail.com>
2021-02-02 23:21:49 -0300

    redirect if isLogged

--- src/routes/LoginRoutes.jsx
+++ src/routes/LoginRoutes.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import LoginCreate from '../components/Login/LoginCreate';
 import LoginForm from '../components/Login/LoginForm';
 import LoginPasswordLost from '../components/Login/LoginPasswordLost';
 import LoginPasswordReset from '../components/Login/LoginPasswordReset';
 
+import { UserContext } from '../stores/UserStore';
+
 export default function LoginRoutes({ match }) {
   let { path } = match;
+
+  const { isLogged } = useContext(UserContext);
+
+  if (isLogged) {
+    return <Redirect to="/conta" />;
+  }
+
   return (
     <Switch>
       <Route path={`${path}/novo`} component={LoginCreate} />
