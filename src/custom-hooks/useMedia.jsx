5bc0a5e72ce36077c37564e53ad0c74fb8bdddb3
JP <jpc.araujo@hotmail.com>
2021-02-04 02:08:38 -0300

    add useMedia hook

---
+++ src/custom-hooks/useMedia.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+export default function useMedia(media) {
+  const [match, setMatch] = useState(null);
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener('resize', changeMatch);
+    return () => {
+      window.removeEventListener('resize', changeMatch);
+    };
+  }, [media]);
+  return match;
+}
