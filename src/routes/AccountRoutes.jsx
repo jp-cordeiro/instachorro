ee214804b4da0d3a3046a634ce00d6960a9708ab
JP <jpc.araujo@hotmail.com>
2021-02-03 22:41:21 -0300

    add and use AccountRoutes

---
+++ src/routes/AccountRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AccountRoutes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import { UserContext } from '../stores/UserStore';
 
 import Home from '../pages/Home';
 import LoginRoutes from './LoginRoutes';
+import ProtectedRoute from '../utils/ProtectedRoute';
 
 const usePrevious = (value) => {
   const ref = React.useRef();
@@ -35,6 +36,7 @@ export default function Router() {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={LoginRoutes} />
+      <ProtectedRoute path="/conta" component={LoginRoutes} />
     </Switch>
   );
 }
