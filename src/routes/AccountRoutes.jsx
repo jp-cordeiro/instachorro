ac6b57566f2d5b61fa12f4eb72796293061a42c3
JP <jpc.araujo@hotmail.com>
2021-02-04 01:05:46 -0300

    use AccountHeaderStore

--- src/routes/AccountRoutes.jsx
+++ src/routes/AccountRoutes.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import AccountHeader from '../pages/Account/AccountHeader';
+import AccountHeaderStore from '../stores/AccountHeaderStore';
+
+import AccountHeader from '../components/Account/AccountHeader';
 import PhotoPost from '../pages/Account/PhotoPost';
 import Statistics from '../pages/Account/Statistics';
 import Feed from '../pages/Account/Feed';
+
+import './styles/account-routes.scss';
 
 export default function AccountRoutes({ match }) {
   let { path } = match;
 
   return (
     <div id="account-pages">
-      <AccountHeader />
+      <AccountHeaderStore>
+        <AccountHeader />
+      </AccountHeaderStore>
       <Switch>
         <Route path={`${path}/postar`} component={PhotoPost} />
         <Route path={`${path}/estatisticas`} component={Statistics} />
