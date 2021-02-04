d8500586218da69ab80d77134ad63d50b0809d33
JP <jpc.araujo@hotmail.com>
2021-02-03 23:40:40 -0300

    refactoring for use useEffect

--- src/utils/ProtectedRoute/index.jsx
+++ src/utils/ProtectedRoute/index.jsx
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { UserContext } from '../../stores/UserStore';
 
 export default function ProtectedRoute(props) {
   const { isLogged } = useContext(UserContext);
-  if (!isLogged) {
-    return <Redirect to="/login" />;
-  } else if (isLogged) {
-    return <Route {...props} />;
-  }
+
+  useEffect(() => {
+    if (!isLogged) {
+      return <Redirect to="/login" />;
+    } else if (isLogged) {
+      return <Route {...props} />;
+    }
+  }, [isLogged]);
+
   return null;
 }
