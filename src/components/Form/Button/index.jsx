6a8f776bfc9cef688aece47e41c4bd7196bd9b74
JP <jpc.araujo@hotmail.com>
2021-02-03 22:27:06 -0300

    remove unused import

--- src/components/Form/Button/index.jsx
+++ src/components/Form/Button/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './button.scss';
 
 export default function Button({ children, ...restProps }) {
   return (
     <div id="btn">
-      <button {...restProps} >{children}</button>
+      <button {...restProps}>{children}</button>
     </div>
   );
 }
